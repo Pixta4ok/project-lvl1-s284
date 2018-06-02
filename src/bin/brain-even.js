37c831eebee7033af00326778077e81d92988744
vv_rustamov <gm.rustamov@gmail.com>
2018-06-02 09:06:57 +0300

    refactor code

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,30 +1,28 @@
 #!/usr/bin/env node
 import askUser from '..';
 
-const sayWelcome = () => console.log('Welcome to Brain Games!');
-const tellGameRules = () => console.log('Answer "yes" if number even otherwise answer "no".\n');
 const getRandomInt = () => Math.floor(Math.random() * (100 - 1)) + 1;
-const isNumberEven = () => {
-  const number = getRandomInt();
-  console.log(`Question: ${number}`);
-  return number % 2 === 0 ? 'yes' : 'no';
-};
-const getUserAnswer = () => askUser('Your answer: ');
+const isNumberEven = number => ((number % 2) === 0 ? 'yes' : 'no');
 const gameLoop = (userName, gameCount) => {
   if (gameCount > 3) {
     console.log(`Congratulations, ${userName}!`);
     return;
   }
 
-  const rightAnswer = isNumberEven();
-  const answer = getUserAnswer();
-  const answerIsCorrect = rightAnswer === answer;
-  console.log(answerIsCorrect ? 'Correct!' : `"${answer}" is wrong answer ;(. Correct answer was "${rightAnswer}".\nLet's try again, ${userName}!`);
-  if (answerIsCorrect) gameLoop(userName, gameCount + 1);
+  const randomNumber = getRandomInt();
+  console.log(`Question: ${randomNumber}`);
+  const rightAnswer = isNumberEven(randomNumber);
+  const userAnswer = askUser('Your answer: ');
+  if (rightAnswer === userAnswer) {
+    console.log('Correct!');
+    gameLoop(userName, gameCount + 1);
+  } else {
+    console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${rightAnswer}". Let's try again, ${userName}!`);
+  }
 };
 
-sayWelcome();
-tellGameRules();
+console.log('Welcome to Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".\n');
 const name = askUser('May I have your name? ');
-console.log(`Hello ${name}!`);
+console.log(`Hello ${name}!\n`);
 gameLoop(name, 1);
